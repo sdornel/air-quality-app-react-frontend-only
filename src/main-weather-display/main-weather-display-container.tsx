63cfc0e679267b7b3b98c6b67194f9aef11971d7
sdornel <kai_dornel@hotmail.com>
2024-04-17 15:10:41 -0400

    fetch more data points

--- src/main-weather-display/main-weather-display-container.tsx
+++ src/main-weather-display/main-weather-display-container.tsx
@@ -7,7 +7,7 @@ import AirQualityInformation from './air-quality-information/air-quality-informa
 import PollutantsInformation from './pollutants-information/pollutants-information';
 
 const MainWeatherDisplayContainer = () => {
-  const dataUrl = 'https://api.openaq.org/v2/locations?limit=1000&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&&dumpRaw=false';
+  const dataUrl = 'https://api.openaq.org/v2/locations?limit=1500&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&&dumpRaw=false';
 
   const airQualityData: MutableRefObject<{}> = useRef({});
   const [loading, setLoading]: Array<boolean | Dispatch<SetStateAction<boolean>>> = useState(false);
@@ -51,21 +51,21 @@ const MainWeatherDisplayContainer = () => {
       <div>
         <h1 className="main-air-quality-display-title">Air Quality Application</h1>
       
-        <div className="main-weather-display-container-div">
+        {/* <div className="main-weather-display-container-div"> */}
 
-          <div className="sidebar left-sidebar">
+          {/* <div className="sidebar left-sidebar">
             <AirQualityInformation />
-          </div>
+          </div> */}
 
-          <div className="world-map-div">
+          {/* <div className="world-map-div"> */}
             <WorldMap airQualityData={airQualityData} getMeasurementData={getMeasurementData} navigate={navigate} onLoad={onLoad} />
-          </div>
+          {/* </div> */}
 
-          <div className="sidebar right-sidebar">
+          {/* <div className="sidebar right-sidebar">
             <PollutantsInformation />
-          </div>
+          </div> */}
 
-        </div>
+        {/* </div> */}
       </div>
     );
 
